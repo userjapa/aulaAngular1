2d7420ba17d75f86e2f0cfcecc4b3f20b2b0d565
usuarioJapa <usuarioJapa>
2017-03-10 18:48:12 -0300

    Add show and btn

--- aulaAngular1/Ex02/index.js
+++ aulaAngular1/Ex02/index.js
@@ -10,7 +10,13 @@ app.controller("IndexController", function($scope) {
      };
     
     $scope.removerDoce = function(doce){
-                
+        $scope.doces.splice($scope.doces.indexOf(doce), 1);                
+    }
+    
+    $scope.mostrarDoces = function() {
+        for (var x = 0; x < $scope.doces.length; x++) {
+            console.log($scope.doces[x]);
+        }
     }
             
 });
