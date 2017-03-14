bc92db01003a79c28cfa1c7688b6d8474b106665
usuarioJapa <willian.hsimabukuro@gmail.com>
2017-03-14 13:30:26 -0300

    Add Remove

--- aulaAngular1/test/js/indexController.js
+++ aulaAngular1/test/js/indexController.js
@@ -4,13 +4,26 @@ angular
     $scope.IndexController = 'IndexController';
     $scope.name = '';
     $scope.city = '';
+    $scope.elemento = {};
     
     $scope.lista = [
         {nome: 'Willian', cidade: 'Cialost'},
-        {nome: 'Lari', cidade: 'Cianorte'},
-        {nome: 'Leo', cidade: 'Cianorte'},
-        {nome: 'Quinalha', cidade: 'Marierva'},
+        {nome: 'Lar', cidade: 'Cianorte'},
+        {nome: 'Is', cidade: 'Cianorte'},
+        {nome: 'Sa', cidade: 'Marierva'},
         {nome: 'Betão', cidade: 'Carrero World'},
         {nome: 'Vivida', cidade: 'Babilon'}
     ];
+    
+    $scope.salvar = function(elemento) {
+        $scope.lista.push($scope.elemento);
+        $scope.elemento = {};
+        console.log($scope.lista);
+    };   
+    
+    $scope.remover = function(elemento) {
+        $scope.lista.splice($scope.lista.indexOf(elemento, 1));
+        console.log($scope.lista);
+    }
+    
     });
